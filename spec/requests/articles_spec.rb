559bce7bc196cc282b364b2bda119f8a3227dceb
colin <cosborn@patientslikeme.com>
2021-12-09 12:22:37 -0700

    implemented test and corrected factory

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -12,15 +12,25 @@ RSpec.describe ArticlesController do
       get articles_path
       expect(json_data.length).to eq(1)
       expected = json_data.first
-      aggregate_failures do 
-        expect(expected[:id]).to eq(article.id)
-        expect(expected[:type]).to eq('article')
+      aggregate_failures do
+        expect(expected[:id]).to eq(article.id.to_s)
+        expect(expected[:type]).to eq('articles')
         expect(expected[:attributes]).to eq(
           title: article.title,
           content: article.content,
           slug: article.slug
         )
       end
+    end
+
+    it 'returns articles in the proper order' do
+      recent_article = create(:article)
+      older_article = create(:article, created_at: 1.hour.ago)
+      get articles_path
+      ids = json_data.map { |item| item[:id].to_i }
+      expect(ids).to(
+        eq([recent_article.id, older_article.id])
+      )
     end
   end
 end
