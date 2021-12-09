46f41de53e154b1b07423c120861485f77320fc9
colin <cosborn@patientslikeme.com>
2021-12-09 16:28:34 -0700

    extracted logic out to concern

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,29 +1,16 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
+  include Paginator
 
   def index
-    articles = Article.recent
-    paginated = paginator.call(
-      articles,
-      params: pagination_params,
-      base_url: request.url
-    )
-    options = { meta: paginated.meta.to_h, links: paginated.links.to_h }
-    render json: serializer.new(paginated.items, options), status: :ok
+    paginated = paginate(Article.recent)
+    render_collection(paginated)
   end
 
   private
 
   def serializer
     ArticleSerializer
-  end
-
-  def paginator
-    JSOM::Pagination::Paginator.new
-  end
-
-  def pagination_params
-    params.permit![:page]
   end
 end
