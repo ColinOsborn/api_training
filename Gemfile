ca9ddf9230e362616dcc33a2e1888a57e60f3c69
colin <cosborn@patientslikeme.com>
2021-12-09 16:14:10 -0700

    passing test but needing refactoring

--- Gemfile
+++ Gemfile
@@ -21,8 +21,8 @@ gem 'puma', '~> 5.0'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
+gem 'jsom-pagination'
 gem 'jsonapi-serializer', '~> 2.2'
-
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,29 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
+
   def index
     articles = Article.recent
-    render json: serializer.new(articles), status: :ok
+    paginated = paginator.call(
+      articles,
+      params: pagination_params,
+      base_url: request.url
+    )
+    options = { meta: paginated.meta.to_h, links: paginated.links.to_h }
+    render json: serializer.new(paginated.items, options), status: :ok
   end
 
   private
 
   def serializer
     ArticleSerializer
+  end
+
+  def paginator
+    JSOM::Pagination::Paginator.new
+  end
+
+  def pagination_params
+    params.permit![:page]
   end
 end
